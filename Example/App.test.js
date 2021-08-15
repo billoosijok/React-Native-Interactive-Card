f58278ea1361dac047ad175ca6627304603bc08d
GateQuality <88676023+GateQuality@users.noreply.github.com>
2021-08-15 21:07:55 +0200

    adding_test

---
+++ Example/App.test.js
@@ -0,0 +1,17 @@
+const App = require("./App")
+// @ponicode
+describe("render", () => {
+    let inst
+
+    beforeEach(() => {
+        inst = new App.default()
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.render()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
